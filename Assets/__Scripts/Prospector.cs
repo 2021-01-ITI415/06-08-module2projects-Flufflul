18b6029f1a168b28a46c1ba27950a63854789fa5
Flufflul <mufflul@gmail.com>
2021-03-03 18:06:40 -0500

    Implement shuffle

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -23,6 +23,21 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+
+		Deck.Shuffle(ref deck.cards);
+
+		Card c;
+		// For all cards in deck, lay the cards evenly between:
+		// 		x = cNum % 13 * 3 : Horizontal layer will never exceed all 13 values
+		// 		y = cNum / 13 * 4 : Vertical layer will incrimentally raise at each cNum factor of 13
+		for (int cNum = 0; cNum < deck.cards.Count; cNum++) {
+			c = deck.cards[cNum];
+			c.transform.localPosition = new Vector3(
+				(cNum % 13) * 3, 
+				cNum / 13 * 4, 
+				0
+			);
+		}
 	}
 
 }
