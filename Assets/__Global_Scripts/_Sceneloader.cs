a25802855598beed9910b9f8812235081b76cce5
Flufflul <mufflul@gmail.com>
2021-03-28 19:33:04 -0400

    Change layout

---
+++ Assets/__Global_Scripts/_Sceneloader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _Sceneloader : MonoBehaviour
+{
+    public void LoadScene_Prospector() { SceneManager.LoadScene("Prospector_Scene_0"); }
+
+    public void LoadScene_Golf() { SceneManager.LoadScene("Golf_Scene_0"); }
+}
